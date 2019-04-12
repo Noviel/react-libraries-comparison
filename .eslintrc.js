0d08acf6014c412b26eca5a9b04a1ce7d4e9f145
Alexander Snov <snov.contact@gmail.com>
2019-04-12 19:05:27 +0700

    Add root readme

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,7 @@
 module.exports = {
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-  // plugins: [
-  //   'react/recommended',
-  //   '@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-  //   'prettier/recommended',
-  // ],
+  parser: '@typescript-eslint/parser',
   extends: [
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'prettier/react',
     'prettier/@typescript-eslint',
@@ -17,22 +12,18 @@ module.exports = {
     },
   },
   parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
+    ecmaVersion: 2018,
+    sourceType: 'module',
     // project: './tsconfig.json',
     // tsconfigRootDir: './',
     extraFileExtensions: ['ts', 'tsx'],
   },
   rules: {
-    // '@typescript-eslint/indent': 'disabled',
     '@typescript-eslint/explicit-member-accessibility': 0,
     '@typescript-eslint/no-explicit-any': 0,
     '@typescript-eslint/no-parameter-properties': 'disabled',
     '@typescript-eslint/no-var-requires': 'disabled',
     '@typescript-eslint/explicit-function-return-type': 'disabled',
     'react/prop-types': 'disabled',
-    // '@typescript-eslint/camelcase': 'disabled',
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
   },
 };
---
+++ README.md
@@ -0,0 +1,8 @@
+# Untitled project
+
+## Technical desicions
+
+- TypeScript as a main language
+- React as an UI layer
+- Monorepo via Yarn workspaces
+- Server side rendering with Next.js and Firebase functions
